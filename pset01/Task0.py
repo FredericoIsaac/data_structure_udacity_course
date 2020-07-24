a9b70fdb3d76fb445333c35ea8e0f24c35aaf9d8
Frederico Gago <fredyisaac@gmail.com>
2020-07-24 05:51:35 +0100

    strating project scramble scv

---
+++ pset01/Task0.py
@@ -0,0 +1,32 @@
+"""
+Read file into texts and calls.
+It's ok if you don't understand how to read files.
+"""
+import csv
+with open('texts.csv', 'r') as f:
+    reader = csv.reader(f)
+    texts = list(reader)
+
+with open('calls.csv', 'r') as f:
+    reader = csv.reader(f)
+    calls = list(reader)
+
+
+"""
+TASK 0:
+What is the first record of texts and what is the last record of calls?
+Print messages:
+"First record of texts, <incoming number> texts <answering number> at time <time>"
+"Last record of calls, <incoming number> calls <answering number> at time <time>, lasting <during> seconds"
+"""
+count = 0
+for lines in texts:
+    print(lines)
+    if count >= 10:
+        break
+    count += 1
+
+texts_incoming_number = texts[0][0]
+texts_answering_number = texts[0][1]
+
+print(texts_incoming_number)
