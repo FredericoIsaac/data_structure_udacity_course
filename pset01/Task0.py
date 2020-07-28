cba557f1879de5287454ee7e85ccb7c8a2a8ac3f
Frederico Gago <fredyisaac@gmail.com>
2020-07-28 06:49:44 +0100

    tanks0

--- pset01/Task0.py
+++ pset01/Task0.py
@@ -19,14 +19,19 @@ Print messages:
 "First record of texts, <incoming number> texts <answering number> at time <time>"
 "Last record of calls, <incoming number> calls <answering number> at time <time>, lasting <during> seconds"
 """
-count = 0
-for lines in texts:
-    print(lines)
-    if count >= 10:
-        break
-    count += 1
+# Getting "texts" info asked
 
 texts_incoming_number = texts[0][0]
 texts_answering_number = texts[0][1]
+texts_time = texts[0][2]
 
-print(texts_incoming_number)
+print("First record of texts, {} texts {} at time {}".format(texts_incoming_number, texts_answering_number, texts_time))
+
+# Getting "calls" info asked
+
+calls_incoming_number = calls[-1][0]
+calls_answering_number = calls[-1][1]
+calls_timestamp = calls[-1][2]
+calls_time = calls[-1][3]
+
+print("Last record of calls, {} calls {} at time {}, lasting {} seconds".format(calls_incoming_number, calls_answering_number, calls_timestamp, calls_time))
---
+++ pset01/time_complexity.md
@@ -0,0 +1,7 @@
+# Time Complexity
+
+>Time complexity of an algorithm quantifies the amount of time taken by an algorithm to run as a function of the length of the input. [***Akash Sharma***]
+
+## Task 0
+
+The runtime in task 0 is **O(n)**, n being the number of lines in the csv file, pass by every line of the file, then we get the values that we want in a order of **O(1)**
