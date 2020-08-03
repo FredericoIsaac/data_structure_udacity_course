f734b63ad77f74af398f91c2bbfa10683e7bfdda
Frederico Gago <fredyisaac@gmail.com>
2020-08-03 05:44:33 +0100

    Task1 finished

--- pset01/Task1.py
+++ pset01/Task1.py
@@ -18,3 +18,18 @@ How many different telephone numbers are there in the records?
 Print a message:
 "There are <count> different telephone numbers in the records."
 """
+
+# Create a set from all the numbers
+numbers_set = set()
+count = 0
+# Get all numbers from texts and add to the set
+for number in texts:
+    numbers_set.add(number[0])
+    numbers_set.add(number[1])
+
+# Get all numbers from calls and add to the set
+for number in calls:
+    numbers_set.add(number[0])
+    numbers_set.add(number[1])
+
+print("There are {} different telephone numbers in the records.".format(len(numbers_set)))
